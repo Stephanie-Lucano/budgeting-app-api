9285850f4dfe5b72c62366f11d3d7c84e1743171
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-20 20:43:40 -0500

    Updated App.js

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // DEPENDENCIES
 const express = require("express");
+const cors = require("cors")
+const transactionsController = require("./controllers/transactionsController")
 
 // CONFIGURATION
 const app = express();
+
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+app.use("/transactions", transactionsController)
 
 // ROUTES
 app.get("/", (request, response) => {
     response.send("Welcome to Budgeting App");
 });
+
+app.get("*", (request, response) => {
+    response.status(404).send("Page not found")
+})
 
 // EXPORT
 module.exports = app;
